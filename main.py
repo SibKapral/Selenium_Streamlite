c7eb7fceb364d2f6fd66e8b464b39efeb8a2118a
Golgolden Thron <dartsitx@gmail.com>
2023-08-29 15:34:54 +0300

    Chanhe7

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import streamlit as st
 import time
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 from bs4 import BeautifulSoup
 
 st.title("Test Selenium")
@@ -19,16 +16,15 @@ XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
 TIMEOUT = 20
 
 
-firefoxOptions = Options()
-firefoxOptions.add_argument("--headless")
-service = Service(GeckoDriverManager().install())
-driver = webdriver.Firefox(
-    options=firefoxOptions,
-    service=service,
-)
-driver.get(URL)
+@st.experimental_singleton
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
 
+driver = get_driver()
 driver.get(URL)
 data = driver.page_source
 
