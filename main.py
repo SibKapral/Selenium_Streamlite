cf3109f1398c5d80b4dd5a563a90f8810b395dfc
Golgolden Thron <dartsitx@gmail.com>
2023-08-29 14:52:27 +0300

    First

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import time
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
+
+st.title("Test Selenium")
+st.markdown("You should see some random Football match text below in about 21 seconds")
+
+URL = "https://www.google.ru/"
+XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
+TIMEOUT = 20
+
+
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
+
+
+driver.get(URL)
+elements = driver.find_elements(By.CLASS_NAME, 'pHiOh')
+
+st.write(elements)
