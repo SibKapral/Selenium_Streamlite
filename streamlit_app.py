dbbabb6f2676e23c40db4977a590ccdb75b73e79
Vladislav <dartsitx@gmail.com>
2023-09-01 00:01:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,6 +112,11 @@ if not st.session_state.df.empty:
                 a = 1
                 with st.status("Parsing..."):
                     st.write("Searching for data...")
+                    try:
+                        driver.out()
+                    except:
+                        print(1)
+                    
                     driver = selen_func(login, passwd)
                     for url_one in urls:
                         driver.get(url_one)
@@ -121,27 +126,3 @@ if not st.session_state.df.empty:
 
 else:
     st.markdown('<h3>Select the data and click the refresh button 😉</h3>', unsafe_allow_html=True)
-
-
-        #if st.button('Send resume', key='button_feedbacks'):
-        #    urls = send_resume(salary_check, min_salary, st.session_state.df, bytes_data, num_vacanc)
-        #    quantity_urls = len(urls)
-        #    a = 1
-        #    with st.spinner("Parsing..."):
-        #        st.write("Searching for data...")
-        #        driver = selen_func(login, passwd)
-        #        for url_one in urls:
-        #            driver.get(url_one)
-        #            st.write(f"{a}/{quantity_urls}")
-        #            a = a+1
-
-
-
-        #with c2:
-        #    if st.button('SEND RESUME', key='button_feedbacks2'):
-        #        df1 = st.session_state.df
-        #        if 'bytes_data' in globals():
-        #            urls = view_resume(salary_check, min_salary, df1, bytes_data, num_vacanc)
-        #        else:
-        #            urls = view_resume(salary_check, min_salary, df1, None, num_vacanc)
-        #        st.dataframe(ur
